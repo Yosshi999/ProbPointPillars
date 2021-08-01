b74fc1f8e7d300ae4e91e425d3c107805e1e197a
Yosshi999 <Yosshi999@users.noreply.github.com>
2021-08-02 01:32:59 +0900

    bugfix: argument num must be int

--- second/utils/eval.py
+++ second/utils/eval.py
@@ -701,7 +701,10 @@ def do_coco_style_eval(gt_annos,
     min_overlaps = np.zeros([10, *overlap_ranges.shape[1:]])
     for i in range(overlap_ranges.shape[1]):
         for j in range(overlap_ranges.shape[2]):
-            min_overlaps[:, i, j] = np.linspace(*overlap_ranges[:, i, j])
+            min_overlaps[:, i, j] = np.linspace(
+                    overlap_ranges[0, i, j],
+                    overlap_ranges[1, i, j],
+                    num=int(overlap_ranges[2, i, j]))
     mAP_bbox, mAP_bev, mAP_3d, mAP_aos = do_eval_v2(
         gt_annos,
         dt_annos,
